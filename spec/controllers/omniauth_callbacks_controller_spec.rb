a0884ac541a30ab3e6a6e4433fa73ba5de3841a4
Igor de Oliveira Sa <igordeoliveirasa@gmail.com>
2014-12-03 00:17:22 -0200

    ok

--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -4,10 +4,28 @@ require 'rails_helper'
 RSpec.describe Users::OmniauthCallbacksController, :type => :controller do
 
 
+  before do
+
+    request.env["devise.mapping"] = Devise.mappings[:user]
+    OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
+                                                                      :provider => 'facebook',
+                                                                      :uid => '123545',
+                                                                      :info => {
+                                                                          :email => 'igordeoliveirasa@gmail.com',
+                                                                          :name => '',
+                                                                          :image => '',
+                                                                      },
+                                                                  })
+
+    request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
+
+  end
+
+
   describe "blabla" do
     it "blabla" do
       get :facebook
-      expect(response).to render_template("facebook")
+
     end
   end
 
