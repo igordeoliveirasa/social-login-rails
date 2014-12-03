37635a2fbaef6b1dc163b75031bc0c44ec691584
Igor de Oliveira Sa <igordeoliveirasa@gmail.com>
2014-12-03 19:44:51 -0200

    ok

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,6 +13,10 @@ require 'coveralls'
 Coveralls.wear!
 
 
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
