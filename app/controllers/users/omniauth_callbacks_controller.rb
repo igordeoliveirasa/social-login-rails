993a125b34449efbda9ac0effde2e578ae146678
Igor de Oliveira Sa <igordeoliveirasa@gmail.com>
2014-12-03 20:17:41 -0200

    refactoring3

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,26 +1,29 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  attr_accessor :session_keys
+
+  @session_keys = {"facebook" => "devise.facebook_data", "google_oauth2" => "devise.google_data",
+                    "twitter" => "devise.twitter_uid", "linkedin" => "devise.linkedin_uid"}
 
   def find_or_create_user(auth)
-    @session_key = ""
-    @user = nil
 
-    if auth.provider == "facebook"
-      @session_key = "devise.facebook_data"
-      @user = User.find_or_create_user(auth.provider, auth.uid, auth.info.name, auth.info.email )
-    elsif auth.provider == "google_oauth2"
-      @session_key = "devise.google_data"
-      @user = User.find_or_create_user(auth.provider, auth.uid, auth.info.name, auth.info.email )
+    name = nil
+    email = nil
+
+    if ["facebook", "google_oauth2"].include?(auth.provider)
+      name = auth.info.name
+      email = auth.info.email
     elsif auth.provider == "twitter"
-      @session_key = "devise.twitter_uid"
+      name = auth.extra.raw_info.name
       email = nil
       if auth.uid
         email = auth.uid + "@twitter.com"
       end
-      @user = User.find_or_create_user(auth.provider, auth.uid, auth.extra.raw_info.name, email)
     elsif auth.provider == "linkedin"
-      @session_key = "devise.linkedin_uid"
-      @user = User.find_or_create_user(auth.provider, auth.uid, auth.info.first_name, auth.info.email)
+      name = auth.info.first_name
+      email = auth.info.email
     end
+
+    @user = User.find_or_create_user(auth.provider, auth.uid, name, email )
   end
 
   def authenticate
@@ -30,7 +33,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => auth.provider) if is_navigational_format?
     else
-      session[@session_key] = request.env["omniauth.auth"]
+      session_key = :session_keys[auth.provider]
+      session[session_key] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
   end
