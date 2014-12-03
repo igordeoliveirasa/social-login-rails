f5255be1d242b3ae71dd46703497cbab612d7c43
Igor de Oliveira Sa <igordeoliveirasa@gmail.com>
2014-12-02 22:21:19 -0200

    ok

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,12 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
+
+
+require 'simplecov'
+SimpleCov.start :rails do
+  add_filter "/spec/"
+end
+
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
