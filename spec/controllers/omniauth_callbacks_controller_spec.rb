6202e8645f20a9df550b0ded8cf558caca35fc38
Igor de Oliveira Sa <igordeoliveirasa@gmail.com>
2014-12-03 00:49:27 -0200

    ok

--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -1,32 +1,41 @@
 require 'rails_helper'
 
+# confira o do Akita: https://github.com/cidadedemocratica/cidadedemocratica/blob/master/spec/controllers/omniauth_callbacks_controller_spec.rb
 
 RSpec.describe Users::OmniauthCallbacksController, :type => :controller do
 
+  describe "login" do
+    before do
+      request.env["devise.mapping"] = Devise.mappings[:user]
+      OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
+                                                                        :provider => 'facebook',
+                                                                        :uid => '123545',
+                                                                        :info => { :email => 'igordeoliveirasa@gmail.com',  :name => '', },
+                                                                    })
 
-  before do
+      request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
+      get :facebook
+    end
 
-    request.env["devise.mapping"] = Devise.mappings[:user]
-    OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
-                                                                      :provider => 'facebook',
-                                                                      :uid => '123545',
-                                                                      :info => {
-                                                                          :email => 'igordeoliveirasa@gmail.com',
-                                                                          :name => '',
-                                                                          :image => '',
-                                                                      },
-                                                                  })
-
-    request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
-
+    it { should be_user_signed_in }
+    it { expect(response).to redirect_to(dashboard_index_path) }
   end
 
-
-  describe "blabla" do
-    it "blabla" do
+  describe "do not login unregistered authorization" do
+    before do
+      request.env["devise.mapping"] = Devise.mappings[:user]
+      OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
+                                                                        :provider => 'facebook',
+                                                                        :uid => '123545',
+                                                                        :info => { :name => '', },
+                                                                    })
+      request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
       get :facebook
-      expect(response).to redirect_to("/dashboard/index")
     end
+
+    it { should_not be_user_signed_in }
+    it { expect(response).to redirect_to(new_user_registration_path) }
+
   end
 
 end
