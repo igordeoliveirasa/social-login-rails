0eabc323b758b166ee7251a8508566d35af2b7d6
Igor de Oliveira Sa <igordeoliveirasa@gmail.com>
2014-12-02 23:35:12 -0200

    ok

--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -1,11 +1,15 @@
 require 'rails_helper'
 
 
-RSpec.describe OmniauthCallbacksController, :type => :controller do
+RSpec.describe Users::OmniauthCallbacksController, :type => :controller do
 
-  it "render facebook" do
-    get :facebook
-    expect(response).to render_template("facebook")
+
+  describe "blabla" do
+    it "blabla" do
+      get :facebook
+      expect(response).to render_template("facebook")
+    end
   end
+
 
 end
