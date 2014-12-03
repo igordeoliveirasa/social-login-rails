d24cce35deff7999ad46769cc44a913a1c41dd26
Igor de Oliveira Sa <igordeoliveirasa@gmail.com>
2014-12-03 00:58:33 -0200

    ok

--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe Users::OmniauthCallbacksController, :type => :controller do
 
-  describe "login" do
+  describe "facebook: login" do
     before do
       request.env["devise.mapping"] = Devise.mappings[:user]
       OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
@@ -21,7 +21,7 @@ RSpec.describe Users::OmniauthCallbacksController, :type => :controller do
     it { expect(response).to redirect_to(dashboard_index_path) }
   end
 
-  describe "do not login unregistered authorization" do
+  describe "facebook: do not login unregistered authorization" do
     before do
       request.env["devise.mapping"] = Devise.mappings[:user]
       OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
@@ -31,6 +31,79 @@ RSpec.describe Users::OmniauthCallbacksController, :type => :controller do
                                                                     })
       request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
       get :facebook
+    end
+
+    it { should_not be_user_signed_in }
+    it { expect(response).to redirect_to(new_user_registration_path) }
+
+  end
+
+  # ========================================================
+
+  describe "google_oauth2: login" do
+    before do
+      request.env["devise.mapping"] = Devise.mappings[:user]
+      OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+                                                                             :provider => 'google_oauth2',
+                                                                             :uid => '123545',
+                                                                             :info => { :email => 'igordeoliveirasa@gmail.com',  :name => '', },
+                                                                         })
+
+      request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+      get :google_oauth2
+    end
+
+    it { should be_user_signed_in }
+    it { expect(response).to redirect_to(dashboard_index_path) }
+  end
+
+  describe "google_oauth2: do not login unregistered authorization" do
+    before do
+      request.env["devise.mapping"] = Devise.mappings[:user]
+      OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+                                                                             :provider => 'facebook',
+                                                                             :uid => '123545',
+                                                                             :info => { :name => '', },
+                                                                         })
+      request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+      get :google_oauth2
+    end
+
+    it { should_not be_user_signed_in }
+    it { expect(response).to redirect_to(new_user_registration_path) }
+
+  end
+
+
+  # ========================================================
+
+  describe "linkedin: login" do
+    before do
+      request.env["devise.mapping"] = Devise.mappings[:user]
+      OmniAuth.config.mock_auth[:linkedin] = OmniAuth::AuthHash.new({
+                                                                             :provider => 'google_oauth2',
+                                                                             :uid => '123545',
+                                                                             :info => { :email => 'igordeoliveirasa@gmail.com',  :name => '', },
+                                                                         })
+
+      request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:linkedin]
+      get :linkedin
+    end
+
+    it { should be_user_signed_in }
+    it { expect(response).to redirect_to(dashboard_index_path) }
+  end
+
+  describe "linkedin: do not login unregistered authorization" do
+    before do
+      request.env["devise.mapping"] = Devise.mappings[:user]
+      OmniAuth.config.mock_auth[:linkedin] = OmniAuth::AuthHash.new({
+                                                                             :provider => 'facebook',
+                                                                             :uid => '123545',
+                                                                             :info => { :name => '', },
+                                                                         })
+      request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:linkedin]
+      get :linkedin
     end
 
     it { should_not be_user_signed_in }
