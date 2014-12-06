77c58d06d19a1f4ca8376f09297a83ed59090f8a
Igor de Oliveira Sa <igordeoliveirasa@gmail.com>
2014-12-06 01:37:28 -0200

    refactoring omniauth_callbacks_controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -22,35 +22,17 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     ret
   end
 
-  def authenticate(auth)
-    # search
-    user = User.find_by_uid(auth["uid"])
+  def authenticate(provider, uid, email)
+    user = User.social_authentication(provider, uid, email)
     unless user
-
-      if auth["email"]
-        user = User.find_by_email(auth["email"])
-        if user
-          uid = Uid.create(provider:auth["provider"], uid:auth["uid"], user:user)
-        end
-      end
-
-      unless user
-        # creating user without e-mail but uid
-        user = User.create(provider:auth["provider"], uid:auth["uid"], name:auth["name"], email:auth["email"], password:Devise.friendly_token[0,20],)
-        uid = Uid.create(provider:auth["provider"], uid:auth["uid"], user:user)
-      end
-    end
-
-    # updating e-mail
-    if auth["email"]
-      user.email = auth["email"]
+      user = User.social_registration(provider, uid, email)
     end
 
     if user.persisted?
       sign_in_and_redirect user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => auth['provider']) if is_navigational_format?
+      set_flash_message(:notice, :success, :kind => provider) if is_navigational_format?
     else
-      session_key = :session_keys[auth['provider']]
+      session_key = :session_keys[provider]
       session[session_key] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
@@ -59,31 +41,31 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     # convert to the same protocol
     auth = protocol_omniauth(request.env["omniauth.auth"])
-    authenticate auth
+    authenticate auth["provider"], auth["uid"], auth["email"]
   end
 
   def google_oauth2
     # convert to the same protocol
     auth = protocol_omniauth(request.env["omniauth.auth"])
-    authenticate auth
+    authenticate auth["provider"], auth["uid"], auth["email"]
   end
 
   def twitter
     # convert to the same protocol
     auth = protocol_omniauth(request.env["omniauth.auth"])
-    authenticate auth
+    authenticate auth["provider"], auth["uid"], auth["email"]
   end
 
   def linkedin
     # convert to the same protocol
     auth = protocol_omniauth(request.env["omniauth.auth"])
-    authenticate auth
+    authenticate auth["provider"], auth["uid"], auth["email"]
   end
 
   def github
     # convert to the same protocol
     auth = protocol_omniauth(request.env["omniauth.auth"])
-    authenticate auth
+    authenticate auth["provider"], auth["uid"], auth["email"]
   end
 
 end
